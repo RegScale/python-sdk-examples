29775b373d1a57ee7f358b9e25c3e1df07dc3248
Bryan Eaton <bryaneaton77@gmail.com>
2024-07-16 14:52:37 -0400

    example tenable job

---
+++ bash_scripts/example_tenable_job.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+SSP=$1
+NESSUS_FOLDER_PATH=$2
+DOMAIN='https://regscale.eatonfam.com'
+
+# Throw exception if SSP is not provided
+
+if [ -z "$SSP" ]
+then
+    echo "SSP is required as first argument"
+    echo "Usage: ./job.sh SSP NESSUS_FOLDER_PATH (optional)"
+    exit 1
+fi
+
+
+export LOGLEVEL=DEBUG
+# Assume we already have our envionment variables set up
+
+# LOGIN
+
+regscale login --username $REGSCALE_USERNAME --password $REGSCALE_PASSWORD --domain $DOMAIN
+
+# sync assets
+
+regscale tenable io sync_assets --regscale_ssp_id $SSP
+
+# Sync vulns
+
+regscale tenable io sync_vulns --regscale_ssp_id $SSP
+
+# Sync Nessus Folder if available
+
+# if nessus folder folder_path is provided, it will sync all nessus folders
+
+if [ -z "$NESSUS_FOLDER_PATH" ]
+then
+    echo "done."
+else
+    regscale tenable io sync_nessus_folder --regscale_ssp_id $SSP --folder_path $NESSUS_FOLDER_PATH
+    echo "done."
+fi
